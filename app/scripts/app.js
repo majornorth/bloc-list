6d212edddb3882abd34706cd9b3d6cff739b0154
Stewart McCoy <mccoy.stewart@gmail.com>
2015-05-13 05:35:42 +0000

    wip setting up ui-router

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,16 @@
-var bloclist = angular.module("BlocList", ["firebase"]);
+var blocList = angular.module("BlocList", ['firebase', 'ui.router']);
 
-bloclist.controller('Landing.controller', ['$scope', '$firebaseArray', function($scope, $firebaseArray) {
+blocList.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
+
+    $stateProvider.state('landing', {
+        url: '/',
+        controller: 'Landing.controller',
+        templateUrl: '/templates/turnup.html'
+    });
+}]);
+
+blocList.controller('Landing.controller', ['$scope', '$firebaseArray', function($scope, $firebaseArray) {
     $scope.subText = "Turn the music up!";
 
     var url = 'https://bloc-list.firebaseio.com/todos';
--- dist/js/app.js
+++ dist/js/app.js
@@ -1,7 +1,17 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-var bloclist = angular.module("BlocList", ["firebase"]);
+var blocList = angular.module("BlocList", ['firebase', 'ui.router']);
 
-bloclist.controller('Landing.controller', ['$scope', '$firebaseArray', function($scope, $firebaseArray) {
+blocList.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
+
+    $stateProvider.state('landing', {
+        url: '/',
+        controller: 'Landing.controller',
+        templateUrl: '/templates/turnup.html'
+    });
+}]);
+
+blocList.controller('Landing.controller', ['$scope', '$firebaseArray', function($scope, $firebaseArray) {
     $scope.subText = "Turn the music up!";
 
     var url = 'https://bloc-list.firebaseio.com/todos';
