a4868d18fa8f1953288e12e648d4ff8f106a9f58
Stewart McCoy <mccoy.stewart@gmail.com>
2015-05-13 06:20:09 +0000

    wip adding todos

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -21,6 +21,21 @@ blocList.controller('Landing.controller', ['$scope', '$firebaseArray', function(
 
     var url = 'https://bloc-list.firebaseio.com/todos';
     var fireRef = new Firebase(url);
+
+    $scope.todos = $firebaseArray(fireRef);
+    $scope.newTodo = '';
+
+    $scope.addTodo = function () {
+        var newTodo = $scope.newTodo.trim();
+        if (!newTodo.length) {
+            return;
+        }
+        $scope.todos.$add({
+            title: newTodo,
+            completed: false
+        });
+        $scope.newTodo = '';
+    };
 }]);
 
 blocList.controller('Second.controller', ['$scope', function($scope) {
